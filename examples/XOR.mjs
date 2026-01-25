f49de6d1a97ca16f3bf1bba365f6b89e75daed73
shabani005 <hasan.shabani2005@gmail.com>
2026-01-25 10:41:39 +0300

    better example

--- examples/XOR.mjs
+++ examples/XOR.mjs
@@ -15,4 +15,9 @@ net.fit(
   [0, 1, 1, 0]
 );
 
+console.log("XOR {0, 0} = ", net.predict([0, 0]));
+console.log("XOR {0, 1} = ", net.predict([0, 1]));
+console.log("XOR {1, 0} = ", net.predict([1, 0]));
+console.log("XOR {1, 1} = ",net.predict([1, 1]));
+
 net.writeJSON("xor.json");
